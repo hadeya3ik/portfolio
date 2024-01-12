e7e6b241fdc04706f54c6c2f150496f476780d3b
hadeya3ik <133555427+hadeya3ik@users.noreply.github.com>
2024-01-12 14:35:34 -0500

    adding typewriter

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,23 +1,50 @@
-import "./hero.css"
-import profile from "./../../images/profile_pic.jpg"
+import { useState, useEffect } from 'react';
+import "./hero.css";
+import profile from "./../../images/profile_pic.jpg";
 import { FaSearch } from "react-icons/fa";
+import Typewriter from 'typewriter-effect';
 
-function hero() {
+function Hero() {
+  const [currentLink, setCurrentLink] = useState("");
+
+  useEffect(() => {
+    const typewriterText = document.querySelector(".Typewriter__wrapper");
+    const observer = new MutationObserver(() => {
+      if (typewriterText.textContent.includes('github.com/hadeya3ik')) {
+        setCurrentLink("https://github.com/hadeya3ik?tab=repositories");
+      } else if (typewriterText.textContent.includes('linkedin.com/hadeya-ikram')) {
+        setCurrentLink("https://www.linkedin.com/in/hadeya-ikram-23197622a/");
+      }
+    });
+
+    observer.observe(typewriterText, { childList: true, subtree: true });
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <div id="hero">
-      <img id="dp" src={profile}></img>
+      <img id="dp" src={profile} alt="Profile"></img>
       <div className="hero_text">
-        <h1 className="bold_style">hi im Hadeya</h1>
+        <h1 className="bold_style">hi, I'm Hadeya</h1>
         <p className="paragraph_style">
-          I enjoy finding ways to combine my creative side with the tech world. I enjoy using my creativity and problem-solving skills to create digital experiences. Whether it's through a striking piece of mixed media art or a clever bit of coding, I'm all about finding the spot where imagination meets innovation.
+        I enjoy finding ways to combine my creative side with the tech world. I enjoy using my creativity and problem-solving skills to create digital experiences. Whether it's through a striking piece of mixed media art or a clever bit of coding, I'm all about finding the spot where imagination meets innovation.
         </p>
-        <button className="linkdin_button" href="https://www.linkedin.com/in/hadeya-ikram-23197622a/">
-          <FaSearch className="search_icon" size={30} /> linkedin.com/hadeya-ikram
-        </button>
+        <div>
+          <a className="linkdin_button" href={currentLink} target="_blank" rel="noopener noreferrer">
+            <FaSearch className="search_icon" size={30} />
+            <Typewriter
+              options={{
+                strings: ['linkedin.com/hadeya-ikram', 'github.com/hadeya3ik'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </a>
+        </div>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default hero
+export default Hero;
