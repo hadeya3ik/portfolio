d254b709b377aacfce60148a0981e63d6b4da76e
hadeya3ik <133555427+hadeya3ik@users.noreply.github.com>
2024-01-13 03:10:52 -0500

    adding dark/light theme option

--- src/components/navbar/navBar.jsx
+++ src/components/navbar/navBar.jsx
@@ -1,5 +1,8 @@
 import { Link, useLocation } from "react-router-dom";
 import "./navbar.css";
+import { MdOutlineLightMode, MdLightMode } from "react-icons/md";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const NavItem = ({ to, children, isActive }) => {
   return (
@@ -18,6 +21,23 @@ const navItems = [
 
 const NavBar = () => {
   const location = useLocation();
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme ? savedTheme === 'dark' : window.matchMedia('(prefers-color-scheme: dark)').matches;
+  });
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-theme', isDarkMode);
+    document.body.classList.toggle('light-theme', !isDarkMode);
+
+    // Save the preference to localStorage
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+  }, [isDarkMode]);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
     <>
       <nav>
@@ -26,10 +46,18 @@ const NavBar = () => {
             <NavItem
               key={item.to}
               to={item.to}
-              isActive={location.pathname === item.to}>
+              isActive={location.pathname === item.to}
+            >
               {item.text}
             </NavItem>
           ))}
+          <li onClick={toggleTheme}>
+            {isDarkMode ? (
+              <MdLightMode size={40} />
+            ) : (
+              <MdOutlineLightMode size={40} />
+            )}
+          </li>
         </ul>
       </nav>
     </>
